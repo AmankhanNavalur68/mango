0a9c57f5dda4b2a729857a008bd83cbd239c3b0e
AmankhanNavalur68 <amankhannavalur@gmail.com>
2026-04-28 18:05:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ st.markdown("""
         <h1 style="color:#2c3e50;">🩺 Skin Disease Analysis & Classification</h1>
         <h3 style="color:#16a085;">AI Powered Deep Learning System</h3>
         <p>Upload a skin image and get instant prediction using Machine Learning</p>
-        <p> aman</p>
+        <p style="color:#16a085;"> aman</p>
     </div>
 """, unsafe_allow_html=True)
